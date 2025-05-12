0554c0b0d3404863d515f01a15d61d30670855a6
Aloula96 <aloula.afewerki@gmail.com>
2025-05-12 15:12:10 +0200

    sixth commit

--- contact.php
+++ contact.php
@@ -1,17 +1,18 @@
 <?php include 'partials/header.php'; ?>
-<main>
+<div class="input__container">
     <h2>Contactez-nous</h2>
     <form action="#" method="post">
+   
         <label for="name">Nom :</label>
         <input type="text" id="name" name="name" required>
-        
+   
         <label for="email">Email :</label>
         <input type="email" id="email" name="email" required>
-        
+    
         <label for="message">Message :</label>
         <textarea id="message" name="message" required></textarea>
         
         <button type="submit">Envoyer</button>
     </form>
-</main>
+    </div>
 <?php include 'partials/footer.php'; ?>
