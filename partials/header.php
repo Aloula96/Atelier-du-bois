96953c44f70f45aeb030f84a9820ed450dbea23b
Aloula96 <aloula.afewerki@gmail.com>
2025-05-12 12:15:20 +0200

    third commit

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+
+    <title>L'Atelier du Bois</title>
+</head>
+<body>
+    
+
 <header>
     <h1>L'Atelier du Bois</h1>
     <nav>
